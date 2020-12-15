c8af135f11407418240ba0e2487ea3cceccf8433
Lonnie Gerol <lonnie@lonniegerol.com>
2020-12-14 19:29:13 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Dining-API
+
+A webscraper + API that returns the hours of operation for the dining locations
+on the RIT campus.
---
+++ scraper.py
@@ -0,0 +1,95 @@
+import urllib.request as request
+from bs4 import BeautifulSoup
+from bs4.element import NavigableString, Tag
+from dataclasses import dataclass
+from datetime import datetime 
+from sys import argv
+
+@dataclass
+class TimeSegment:
+    """
+    A data class that represents a segment of time for which a dining location 
+    is open. If a location is open for lunch and dinner the time for those two
+    'segments' is in two different tables on the dining website.
+    """
+    hours: str
+    # segment_name: str
+
+@dataclass
+class Location:
+    """ 
+    A data class that represents a dining location on campus
+    """
+    location_name: str
+    segments: [TimeSegment]
+
+def pretty_print_locations(locations):
+    """ 
+    Pretty prints the hours of a all dining locations in a human readable
+    format. 
+    """
+    for location in locations:
+        print(location.location_name)
+        print("=====")
+        for time_segment in location.segments:
+            print(time_segment.hours)
+        print("\n")
+
+def new_location(name):
+    """Creates a new location dataclass with the specified name"""
+    return Location(name, [])
+
+def fetch_locations(day, month, year):
+    """ fetches the hours for all dining locations on the given date. """
+    hours_page = "https://www.rit.edu/fa/diningservices/places-to-eat/hours?date=" + str(year) + "-" + str(month) + "-" + str(day) + "&format=day"
+    lst = []
+    hour_class = "hours-title"
+    location_container_class = "view-places-to-eat"
+    
+    page = request.urlopen(hours_page)
+    
+    soup = BeautifulSoup(page, 'html.parser')
+    
+    location_container = soup.find("div", class_="view-content")
+   
+    location_name = location_container.contents[3].text.strip()
+    location = Location(location_name, []) 
+    for item in location_container.contents[4:]:
+        # Ignoring NavigableStrings, not sure why they are here in the first place
+        if isinstance(item, Tag):
+            # Tags with hours-title class only have one class         
+            if item['class'][0] == "hours-title":
+                lst.append(location)
+                location_name = item.text.strip()
+                location = new_location(location_name)
+                
+            else:
+                # Todo: rename this variable!
+                close = item.find("div", class_="col-sm-5")
+                if close is not None:
+                    hours = str(close.contents[0]).strip()
+                else:
+                    hours = "Closed"
+
+                location.segments.append(TimeSegment(hours))
+    lst.append(location)
+    return lst
+
+def main():
+
+    if len(argv) > 1:
+        month_str = argv[1]
+        day_str = argv[2]
+        year_str = argv[3]
+    else: 
+        todays_date = datetime.now()
+        day_str = str(todays_date.day)
+        month_str = str(todays_date.month)
+        year_str = str(todays_date.year)
+
+    locations = fetch_locations(day_str, month_str, year_str) 
+    print("\n==" + month_str + "/" + day_str + "/" + year_str + "==\n")
+    pretty_print_locations(locations)
+
+if __name__ == '__main__':
+    main()
