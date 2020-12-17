4466ae4b576a1dac626a38b3ce3d846ce2798c3e
Lonnie Gerol <lonnie@lonniegerol.com>
2020-12-16 19:25:29 -0500

    attempted to fix threading issue

--- wsgi.py
+++ wsgi.py
@@ -4,4 +4,4 @@ Primary entry point for the app
 from dining import app
 
 if __name__ == '__main__':
-    app.run(host=app.config['IP'], port = int(app.config['PORT']))
+    app.run(host=app.config['IP'], port = int(app.config['PORT']), threaded=True)
